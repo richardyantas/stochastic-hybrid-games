5692e713fba4bf4e1e87d03c258891e6c4a84a2c
richardyantas5 <richadyantas5@gmail.com>
2021-02-06 23:32:57 -0500

    some paths in config ware changed

--- lib/config/jsonu.h
+++ lib/config/jsonu.h
@@ -4,7 +4,7 @@
 // #else
 // #include <jsoncpp/json/json.h>
 // #endif
-#include "../lib/jsoncpp/include/json/json.h"
+#include "../jsoncpp/include/json/json.h"
 
 struct JsonConversionException : public std::exception {
     const char *what() const noexcept override { return "Cannot convert json to object"; }
