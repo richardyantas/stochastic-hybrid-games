9c9c4c33528cf4529ca78085c9b9f6ab11a37ed2
richardyantas5 <richadyantas5@gmail.com>
2021-02-07 22:49:27 -0500

    Zonotope methods to export in uppaal format added in jsonParseUppaal

--- sources/patterns/jsonParseUppaal.py
+++ sources/patterns/jsonParseUppaal.py
@@ -9,26 +9,35 @@ def toBracesString(data):
     b = []
     for zonotopePat in zonotopePats:
       b.append("{"+",".join(str(e) for e in zonotopePat)+"}")    
-    a.append("{"+ ",".join(b) + "}\n" )
-  return "{"+ ",".join(a) + "}"
+    a.append("\n{"+ ",".join(b) + "}" )
+  return "{"+ ",".join(a) + "\n}"
   
 def jsonParseUppaal():
-  f = open("jsonToUppal.txt")  
-  with open("pattern.json") as json_file:
+  f = open("jsonToUppal.txt", "w+")  
+  with open("patterns_test.json") as json_file:
     data = json.load(json_file)
     K = data["K"]    
     # add export file
     M=0
-    for zonotopePats in data["x"]:
+
+    p = []
+    for zonotope in data["zonotope"]:
+      q = []
+      for zonotopeState in zonotope:
+        q.append("{"+",".join(str(e) for e in zonotopeState)+"}")
+      p.append("\n{"+ ",".join(q) + "}")
+    zonotopes = "\n{"+ ",".join(p) + "\n}"
+
+    for zonotopePats in data["patterns"]:
       M=max(len(zonotopePats),M)
     print("M:", M)
-    for zonotopePats in data["x"]: # x is patterns
+    for zonotopePats in data["patterns"]:
       for zonotopePat in zonotopePats:
         while len(zonotopePat)<K:
           zonotopePat.append(-1)
-      while len(zonotopePats)<M:   # calculate the max
-        zonotopePats.append([-2,-2,-2])
-
-    print( toBracesString(data["x"]) )
+      while len(zonotopePats)<M:   
+        zonotopePats.append([-2,-2,-2])    
+  f.write(toBracesString(data["patterns"]))
+  f.write(zonotopes)
 
 jsonParseUppaal()
