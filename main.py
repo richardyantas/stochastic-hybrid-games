789204523399bd972559aaef93fa6a4620617922
richardyantas5 <richadyantas5@gmail.com>
2021-04-02 18:15:19 -0500

    main.py is added and also interval package was added

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from sthocastic_hybrid_game.src.dataProcessing import DataProcessing
+from sthocastic_hybrid_game.src.simulation import simulation
+from sthocastic_hybrid_game.src.config import *
+
+data = DataProcessing(filepath1, filepath2)
+print(data.getDisturbanceSeries())
+simulation("normal", [0.0, 0.11, 42.0], data.getDisturbanceSeries())
+
