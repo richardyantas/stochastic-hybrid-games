3bfe7595133d0cb58599214e87ef39f68b18d2c9
richardyantas5 <richadyantas5@gmail.com>
2021-04-06 10:47:29 -0500

    model for the case study is added on model.py but is not still tested to interval(0,1)*

--- sources/patterns/jsonUtils.h
+++ sources/patterns/jsonUtils.h
@@ -5,7 +5,7 @@ template<class T>
 Json::Value jvec1D(vector<T> in)
 {
   Json::Value jvec;
-  for (int i = 0; i < in.size(); i++)
+  for (unsigned int i = 0; i < in.size(); i++)
   {
     jvec.append(in[i]);
   }
@@ -16,7 +16,7 @@ template<class T>
 Json::Value jvec2D(vector<vector<T>> in)
 {
   Json::Value jvec;
-  for (int i = 0; i < in.size(); i++)
+  for (unsigned int i = 0; i < in.size(); i++)
   {
     jvec.append( jvec1D<T>(in[i]));
   }
