8fe8e5ba097d911a7022dc66f0f78cc160deecda
richardyantas5 <richadyantas5@gmail.com>
2021-06-04 04:17:34 -0500

    data python class were created to manage and process the input data

--- sthocastic_hybrid_game/src/data/SOLAR.py
+++ sthocastic_hybrid_game/src/data/SOLAR.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import random
 import argparse
 from sthocastic_hybrid_game.src.data.base_data_module import BaseDataModule
 
 DATA_DIR = BaseDataModule.data_dirname()
+
 WATER_TEMPERATURE_INPUT = 25
 
 
@@ -29,7 +31,20 @@ class SOLAR(BaseDataModule):
         Te = np.interp(t, previous_data["t"], previous_data["Te"])
         I = np.interp(t, previous_data["t"], previous_data["I"])
         Ti = np.ones(new_num_intervals + 1) * WATER_TEMPERATURE_INPUT
-        return {"Te": Te, "I": I, "Ti": Ti}
+        return {"Te": Te, "I": I, "Ti": Ti, "t": t}
+
+    def uncontrollable_action_generation(self):
+        # valves for water consumption on buildings , tau = 5 !!! IMPORTANT to link with the general tau from controller in some way
+        print("dt:", self.dt)
+        U_MODES = (np.zeros(int(self.life_time/(5*60)))).tolist()
+        num_actions = random.randrange(5, 8)
+        standard_deviation = 1*3  # 1*60/5
+        for i in range(0, num_actions):
+            U_MODES[int(random.gauss(7*60/5, standard_deviation))] = 1
+            U_MODES[int(random.gauss(13*60/5, standard_deviation))] = 1
+            U_MODES[int(random.gauss(19*60/5, standard_deviation))] = 1
+        print("umodes:", len(U_MODES))
+        return U_MODES
 
     def loader_data(self):
         data = self.transform_data()
