5b7708233d62cb1e6ef8a6186e80d605db8bdbe3
richardyantas5 <richadyantas5@gmail.com>
2021-06-20 19:24:16 -0500

    refactored MPC and works

--- sthocastic_hybrid_game/src/controllers/MPC.py
+++ sthocastic_hybrid_game/src/controllers/MPC.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 import json
 import numpy as np
 import argparse
+import random
 import multiprocessing
 from interval import interval
 from sthocastic_hybrid_game.src.data.base_data_module import BaseDataModule
@@ -12,7 +13,9 @@ DATA_DIR = BaseDataModule.data_dirname()
 SAFE_RES = json.load(open(f"{DATA_DIR}/pattern.json"))
 PATTERNS = SAFE_RES["patterns"]
 ZONOTOPES = SAFE_RES["zonotopes"]
-TAU = SAFE_RES["tau"]  # 300 should be fixed by safe patterns
+# TAU = SAFE_RES["tau"]  # 300 should be fixed by safe patterns
+SAFE_DATA = json.load(open(f"{DATA_DIR}/static_data.json"))
+TAU = SAFE_DATA["tau"]  # 300 should be fixed by safe patterns
 
 
 def query_safe_patterns(state):
@@ -29,55 +32,57 @@ def query_safe_patterns(state):
 
 class MPC():
     def __init__(self, model: Any, data_config: Dict[str, Any], disturbs: Dict[str, Any], args: argparse.Namespace = None):
-        #self.tau = self.args.get("tau", TAU)
+        self.Te = disturbs["Te"]
+        self.Ti = disturbs["Ti"]
+        self.I = disturbs["I"]
+        self.t = disturbs["t"]
         self.tau = TAU
         self.model = model
-        self.state = self.model.get_initial_state()
-        self.pat = list(self.optimal_pattern_search(
-            query_safe_patterns(self.state)))
-        self.modes = self.model.get_controllable_modes()
         self.u_actions = self.model.get_uncontrollable_actions()
+        self.initial_state = self.model.get_initial_state()
+        self.nrSteps = self.model.get_number_steps()
+        self.pat = list(query_safe_patterns(list(self.initial_state))[0])
+        #self.controllable_mode = -1
+        self.c_actions = []  # [C_MODES[self.controllable_mode]]
         self.queue = multiprocessing.Queue()
-        self.process = multiprocessing.Process(
-            target=self.predict, args=(self.pat.pop(0), self.state, 0))
-        # states to plot
-        self.c_actions = [self.modes[self.pat[-1]]]
-        #self.u_actions = []
-        self.states = [self.state]
+        self.H = self.nrSteps*self.tau
 
     def optimal_pattern_search(self, patterns):
         # ANN controller
         #print("pattern choosen: ", patterns[1])
-        return patterns[1]  # for the moment
+        pattern_size = len(patterns)
+        pos = random.randrange(0, pattern_size)
+        return patterns[0]  # for the moment
 
 #  ############################# CAMBIAR TAU sec -> min ###################
 
-    def predict(self, mode, state, index):
+    def predict(self, controllable_mode, state, index):
         predicted_state = state
-        taumin = int(self.tau/60)
-        for i in range(index, index + taumin):
-            predicted_state = self.model.post(mode, predicted_state, index)
+        for i in range(index, index + self.tau):
+            predicted_state = self.model.post(
+                controllable_mode, self.u_actions[index], predicted_state, i)  # antes era index
         patterns = query_safe_patterns(predicted_state)
         optimal_pattern = self.optimal_pattern_search(patterns)
         self.queue.put(optimal_pattern)
         return
 
-    def control(self, index):
+    def control(self, state, index):
         if(len(self.pat) == 0):
             self.pat = self.queue.get()
-            controllable_mode = self.pat.pop(0)
+            self.controllable_mode = self.pat.pop(0)
         elif(len(self.pat) == 1):
-            controllable_mode = self.pat.pop(0)
+            self.controllable_mode = self.pat.pop(0)
             process = multiprocessing.Process(target=self.predict, args=(
-                controllable_mode, self.state, index))
+                int(self.controllable_mode), state, index))
             process.start()
         elif(len(self.pat) > 1):
-            controllable_mode = self.pat.pop(0)
+            self.controllable_mode = self.pat.pop(0)
+        print("W")
+        self.c_actions.append(C_MODES[self.controllable_mode])
+        return self.controllable_mode
 
-        self.state = self.model.update(controllable_mode, self.state, index)
-        self.c_actions.append(list(self.modes[controllable_mode]))
-        self.states.append(list(self.state))
-        return self.pat
+    def get_nrSteps(self):
+        return self.nrSteps
 
     def get_tau(self):
         return self.tau
@@ -85,14 +90,8 @@ class MPC():
     def get_pat(self):
         return self.pat
 
-    def get_states(self):
-        return self.states
-
     def get_controllable_actions(self):
         return self.c_actions
-
-    def get_uncontrollable_actions(self):
-        return self.u_actions
 
     @ staticmethod
     def add_to_argparse(parser):
