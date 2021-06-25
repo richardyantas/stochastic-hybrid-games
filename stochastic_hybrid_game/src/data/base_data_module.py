bd9925ff5f0d9f2ca86e59b0587d2fd62eaf404e
richardyantas5 <richadyantas5@gmail.com>
2021-06-24 19:40:44 -0500

    Start_time defined on base_data_module as 1*24*60 and life_time as 2*24*60

--- stochastic_hybrid_game/src/data/base_data_module.py
+++ stochastic_hybrid_game/src/data/base_data_module.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import argparse
 
-LIFE_TIME = 3*24*60  # (in min) two days  BATCH_SIZE
+LIFE_TIME = 2*24*60  # 3*24*60 # (in min) two days  BATCH_SIZE
+START_TIME = 1*24*60
 DATA_SAMPLE_TIME = 15  # (in min)
 DT = 1  # data_resample_time (in min)
 
@@ -14,6 +15,8 @@ DT = 1  # data_resample_time (in min)
 class BaseDataModule():
     def __init__(self, args: argparse.Namespace = None) -> None:
         self.args = vars(args) if args is not None else {}
+        self.start_time = int(self.args.get(
+            "start_time", START_TIME))
         self.life_time = int(self.args.get(
             "life_time", LIFE_TIME))
         self.data_sample_time = self.args.get(
@@ -25,10 +28,11 @@ class BaseDataModule():
         return Path(__file__).resolve().parents[2]/"datasets"
 
     def config(self):
-        return {"life_time": self.life_time, "data_sample_time": self.data_sample_time, "dt": self.dt}
+        return {"start_time": self.start_time, "life_time": self.life_time, "data_sample_time": self.data_sample_time, "dt": self.dt}
 
     @staticmethod
     def add_to_argparse(parser):
+        parser.add_argument("--start_time", type=int, default=START_TIME)
         parser.add_argument("--life_time", type=int, default=LIFE_TIME)
         parser.add_argument("--data_sample_time", type=int,
                             default=DATA_SAMPLE_TIME)
